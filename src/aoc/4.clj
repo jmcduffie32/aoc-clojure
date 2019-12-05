f20a9cb22dd01351a0d2b5c9bcab55eee8e607ea
Jon McDuffie <bobber321988@gmail.com>
2019-12-04 21:44:54 -0500

    Finish day 4

--- src/aoc/4.clj
+++ src/aoc/4.clj
@@ -2,7 +2,6 @@
 
 (def range-min 307237)
 (def range-max 769058)
-;; 366666 possibilites
 
 (defn increments? [number]
   (-> number
@@ -13,42 +12,18 @@
            (filter (fn [pair] (< (second pair) (first pair)))))
       (empty?)))
 
-(defn has-a-double? [number]
+(defn get-digit-counts [number]
   (-> number
       (str)
       (str/split #"")
-      (->> (mapv clojure.edn/read-string)
-           (partition 2 1)
-           (filter (fn [pair] (= (second pair) (first pair)))))
-      (not (partial empty?))))
-
-(defn has-no-five-repeats? [number]
-  (-> number
-      (str)
-      (str/split #"")
-      (->> (partition 5 1)
-           (some (partial apply =)))
-      (not)))
-
-(defn all-same-digits? [number]
-  (-> number
-      (str)
-      (str/split #"")
-      (->>(apply =))))
-
-(defn not-all-same-digits? [number]
-  (-> number
-      (str)
-      (str/split #"")
-      (->>(apply =))
-      (not)))
+      (->> (partition-by identity)
+           (map count))))
 
 
 (defn solve [range-min range-max]
   (->> (range range-min range-max)
        (filter #(and
-                 (has-no-five-repeats? %)
-                 (not-all-same-digits? %)
-                 (has-a-double? %)
+                 (->> (get-digit-counts %)
+                      (some #{2}))
                  (increments? %)))
        (count)))
