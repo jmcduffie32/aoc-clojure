7130cbb2fecf3f1bd9386ecb4c7ea9070f6e4744
Jon McDuffie <bobber321988@gmail.com>
2019-12-06 20:07:31 -0500

    Complete day 6

--- src/aoc/6.clj
+++ src/aoc/6.clj
@@ -1,26 +1,36 @@
 (ns aoc.6
   (:require [clojure.string :as str]))
 
+(def input-file "./resources/6_1.txt")
+;; (def input-file "./resources/6_test.txt")
+
 (defn get-orbit-map []
-  (-> (slurp "./resources/6_1.txt")
-      (str/split #"\n")
+  (-> (slurp input-file)
+      (str/split-lines)
       (->> (map #(str/split % #"\)"))
-           (map (fn [val] [(keyword (first val)), (keyword (second val))]))
            (reduce (fn [dict orbit]
                      (assoc dict
-                             (second orbit)
-                             (first orbit)))
+                            (second orbit)
+                            (first orbit)))
                    {}))))
 (def orbit-map (get-orbit-map))
-;; (defn solve []
-;;   (->> 
-;;        ))
 (defn get-orbits [planet orbit-list]
-  (let [next-planet (planet orbit-map)
+  (let [next-planet (get orbit-map planet)
         new-list (conj orbit-list next-planet)]
-    (if (= planet :COM)
-      (filter (fn [v] (not (nil? v))) new-list)
+    (if (= next-planet "COM")
+      new-list
       (recur next-planet new-list))))
 
 (defn solve []
-  (into {} (for [[k v] orbit-map] {k (get-orbits k [])})))
+  (->> (into [] (for [[k v] orbit-map]
+                 (if (not (= "COM" k))
+                   (count (get-orbits k [])))))
+       (apply +)))
+
+(def you-orbits (into #{} (get-orbits "YOU" [])))
+(def santa-orbits (into #{} (get-orbits "SAN" [])))
+
+(defn solve2 []
+  (-> (clojure.set/difference you-orbits santa-orbits)
+      (clojure.set/union (clojure.set/difference santa-orbits you-orbits))
+      (count)))
