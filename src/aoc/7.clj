7de56a38e08702576bc6ac1daeff5bdce62d26d1
Jon McDuffie <bobber321988@gmail.com>
2019-12-07 15:42:22 -0500

    Solve day 7 part 2

--- src/aoc/7.clj
+++ src/aoc/7.clj
@@ -67,7 +67,6 @@
       (= opcode 1) (let [val1 (get-val program (+ index 1) arg1-mode)
                          val2 (get-val program (+ index 2) arg2-mode)
                          result-index (nth program (+ index 3))]
-                     (println "val1" val1 "val2" val2)
                      (-> state
                          (assoc :program (assoc program result-index (+ val1 val2)))
                          (assoc :index (+ index 4))
@@ -117,7 +116,8 @@
                          (assoc :program (assoc program result-index result))
                          (assoc :index (+ index 4))
                          (recur)))
-      (= opcode 99) state
+      (= opcode 99) (-> state
+                        (assoc :has-halted true))
       :else (do (println "INVALID OP CODE: " opcode) program))))
 
 (defn get-thrust [phase-settings input]
@@ -133,6 +133,29 @@
       (:output output-state)
       (recur rest (:output output-state)))))
 
+(defn assign-phase-settings [amps phase-settings]
+  (-> amps
+   (assoc-in [0 :phase] (nth phase-settings 0))
+   (assoc-in [1 :phase] (nth phase-settings 1))
+   (assoc-in [2 :phase] (nth phase-settings 2))
+   (assoc-in [3 :phase] (nth phase-settings 3))
+   (assoc-in [4 :phase] (nth phase-settings 4))))
+
+(defn get-feedback-thrust [phase-settings]
+  (loop [i 0
+         amps (assign-phase-settings amps phase-settings)]
+    (let [amp-index (mod i (count amps))
+          next-amp-index (mod (inc amp-index) (count amps))
+          new-amp-state (-> (nth amps amp-index)
+                           (run-prog))]
+      ;; (println "INDEX:" amp-index
+      ;;          "PROGRAM:" new-amp-state)
+      (if (:has-halted new-amp-state)
+        (:input new-amp-state)
+        (recur (inc i) (-> amps
+                           (assoc amp-index new-amp-state)
+                           (assoc-in [next-amp-index :input] (:output new-amp-state))))))))
+
 (defn solve []
   (->> (combo/permutations [0 1 2 3 4])
        (map (fn [setting] (get-thrust setting 0)))
@@ -140,5 +163,5 @@
 
 (defn solve2 []
   (->> (combo/permutations [5 6 7 8 9])
-       (map (fn [setting] (get-thrust setting 0)))
+       (map (fn [setting] (get-feedback-thrust setting)))
        (apply max)))
