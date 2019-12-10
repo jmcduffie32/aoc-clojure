9a17fff0b3aaa942bc89fdf1600453e1a836add6
Jon McDuffie <jon.mcduffie@vonage.com>
2019-12-10 18:03:39 -0500

    day 10

--- src/aoc/10.clj
+++ src/aoc/10.clj
@@ -1,11 +1,42 @@
-(ns aoc.10 (:require [clojure.string :as str]))
+(ns aoc.10 (:require [clojure.math.combinatorics :as combo]
+                     [clojure.string :as str]))
 
-(def input-file "./resources/10_test.txt")
+(def input-file "./resources/10_1.txt")
+;; (def input-file "./resources/10_test.txt")
 
-(defn solve []
+(def asteroid-map
   (-> input-file
       (slurp)
-      (str/split-lines)))
+      (str/split-lines)
+      (->> (mapv #(str/split % #"")))))
 
+(def x-dim (count (first asteroid-map)))
+(def y-dim (count asteroid-map))
 
+(def points
+  (->> (for [y (range y-dim)
+             x (range x-dim)]
+         (if (= (nth (nth asteroid-map y) x) "#")
+           {:x x :y y}))
+       (filter not-empty)))
 
+(defn calc-slope [point1 point2]
+  (let [x1 (:x point1)
+        x2 (:x point2)
+        y1 (:y point1)
+        y2 (:y point2)]
+    (if (= x1 x2)
+      :inf
+      (/ (- y2 y1) (- x2 x1)))))
+
+(def combinations (combo/combinations points 2))
+(def sorted-combinations (sort-by #(:y (first %)) (sort-by #(:x (first %)) combinations)))
+(def grouped-combinations (partition-by (fn [points] {:x (:x (first points))
+                                                      :y (:y (first points))})
+                                        sorted-combinations))
+(defn calc-slopes [combinations]
+  (map #(apply calc-slope %) combinations))
+(defn solve []
+  (->> grouped-combinations
+       (map get-visible)
+       (apply max)))
