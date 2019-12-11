7d008287adbe2a76e9d5ab190ad7b320cb0daad9
Jon McDuffie <bobber321988@gmail.com>
2019-12-10 21:23:12 -0500

    solve day 10

--- src/aoc/10.clj
+++ src/aoc/10.clj
@@ -25,9 +25,20 @@
         x2 (:x point2)
         y1 (:y point1)
         y2 (:y point2)]
-    (if (= x1 x2)
-      :inf
-      (/ (- y2 y1) (- x2 x1)))))
+    (cond
+      (and (= x1 x2) (> y2 y1)) Double/POSITIVE_INFINITY
+      (and (= x1 x2) (< y2 y1)) Double/NEGATIVE_INFINITY
+      :else (if (> x2 x1)
+              (str "<" (/ (- y2 y1) (- x2 x1)))
+              (str ">" (/ (- y2 y1) (- x2 x1)))))))
+
+(defn negate-slope [slope]
+  (cond
+    (= slope Double/POSITIVE_INFINITY) Double/NEGATIVE_INFINITY
+    (= slope Double/NEGATIVE_INFINITY) Double/POSITIVE_INFINITY
+    :else (if (= \< (nth slope 0))
+            (str ">" (drop 1 slope))
+            (str "<" (drop 1 slope)))))
 
 (def combinations (combo/combinations points 2))
 (def sorted-combinations (sort-by #(:y (first %)) (sort-by #(:x (first %)) combinations)))
@@ -35,8 +46,36 @@
                                                       :y (:y (first points))})
                                         sorted-combinations))
 (defn calc-slopes [combinations]
-  (map #(apply calc-slope %) combinations))
+  (->> combinations
+       (map #(apply calc-slope %))))
+
+(defn get-visible [combinations]
+  (->> combinations
+       (map #(apply calc-slope %))
+       (into #{})
+       (count)))
+
+(defn create-slope-map []
+  (loop [combinations combinations
+         state {}]
+    (let [[curr & rest] combinations
+          [point1 point2] curr
+          key1 (str (:x point1) "," (:y point1))
+          key2 (str (:x point2) "," (:y point2))
+          slope (calc-slope point1 point2)]
+      (if (empty? rest)
+        (-> state
+            (update key1 #(conj % slope))
+            (update key2 #(conj % (negate-slope slope))))
+        (recur rest (-> state
+                        (update key1 #(conj % slope))
+                        (update key2 #(conj % (negate-slope slope)))))))))
+(def slope-map (create-slope-map))
+
 (defn solve []
-  (->> grouped-combinations
-       (map get-visible)
+  (->> slope-map
+       (vals)
+       (map #(into #{} %))
+       (map count)
        (apply max)))
+
